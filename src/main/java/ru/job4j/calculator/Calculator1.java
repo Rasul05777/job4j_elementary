d2070538a69c3ea21ce8d5c075ac4273b13471be
Rasul05777 <96072754+Rasul05777@users.noreply.github.com>
2023-05-06 01:47:37 +0300

    4.2. Результат работы метода [#232622]

---
+++ src/main/java/ru/job4j/calculator/Calculator1.java
@@ -0,0 +1,12 @@
+package ru.job4j.calculator;
+
+public class Calculator1 {
+    public static void func1(int first, int second) {
+        int result3 = first + second;
+        System.out.println(result3);
+    }
+
+    public static void main(String[] args) {
+        Calculator1.func1(50, 50);
+    }
+}
