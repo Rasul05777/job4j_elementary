ac0ae447637e183ae9c176e846295ee0b81f7e10
Rasul05777 <96072754+Rasul05777@users.noreply.github.com>
2023-07-16 19:27:22 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -5,8 +5,13 @@ public class Max {
         return left > right ? left : right;
     }
 
+    public static int max(int left, int right, int up) {
+        int one = max(right, up);
+        return one > left ? one : left;
+    }
+
     public static void main(String[] args) {
-        int result = Max.max(10, 5);
+        int result = Max.max(15, 5, 21);
         System.out.println("Наибольшее из двух чисел = " + result);
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -5,28 +5,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class MaxTest {
     @Test
-    public void whenMax1To2Then2() {
+    public void whenMax1To2To11Then11() {
         int left = 1;
         int right = 2;
-        int result = Max.max(left, right);
-        int expected = 2;
+        int one = 11;
+        int result = Max.max(left, right, one);
+        int expected = 11;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    public void whenMax5To1Then5() {
+    public void whenMax5To1To1Then5() {
         int left = 5;
         int right = 1;
-        int result = Max.max(left, right);
+        int one = 1;
+        int result = Max.max(left, right, one);
         int expected = 5;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    public void whenMax5To5Then5() {
+    public void whenMax5To5To5Then5() {
         int left = 5;
         int right = 5;
-        int result = Max.max(left, right);
+        int one = 5;
+        int result = Max.max(left, right, one);
         int expected = 5;
         assertThat(result).isEqualTo(expected);
     }
